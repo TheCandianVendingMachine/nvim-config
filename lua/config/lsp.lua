9145ebf5d808046f790bfba99e6277b914ba0828
Bailey Danyluk <bailey.d@athinkingape.com>
2023-05-08 16:40:47 -0600

    add diagnostic keybinds

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -19,3 +19,7 @@ require('rust-tools').setup({
         auto = true,
     }
 })
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', ']e', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[e', vim.diagnostic.goto_prev)
