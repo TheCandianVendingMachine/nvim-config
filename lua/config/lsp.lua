c262852e3804648c0bba910d3e6dc37e78237ede
Bailey Danyluk <baileydanyluk@gmail.com>
2023-09-14 20:27:20 -0600

    Add C LSP:

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -25,6 +25,8 @@ require('rust-tools').setup({
     }
 })
 
+lsp['clangd'].setup({})
+
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', ']e', vim.diagnostic.goto_next)
 vim.keymap.set('n', '[e', vim.diagnostic.goto_prev)
--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 local ts = require 'nvim-treesitter.configs'
 ts.setup {
-    ensure_installed = { "python", "typescript", "rust", "lua", "html", "dockerfile", "markdown" },
+    ensure_installed = { "python", "typescript", "rust", "c", "lua", "html", "dockerfile", "markdown" },
     sync_install = false,
     auto_install = true,
 
