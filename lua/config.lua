85f03f44546061d6e07a305a03ddc30d9b325d94
Bailey <baileydanyluk@gmail.com>
2025-10-18 16:33:47 -0600

    date

--- lua/config.lua
+++ lua/config.lua
@@ -11,12 +11,8 @@ vim.diagnostic.config({
     severity_sort = true,
     update_in_insert = false,
     signs = true,
-    virtual_lines = {
-        current_line = true
-    },
     virtual_text = {
-        prefix = '-',
-        current_line = false,
+        prefix = '#',
     },
     float = {
         border = 'rounded',
